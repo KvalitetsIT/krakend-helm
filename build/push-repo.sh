51821bafb50ea9c392d08a7f3d57eeafc8662543
evtr <eva@kvalitetsit.dk>
2022-06-27 08:44:12 +0200

    Update push-repo.sh

--- build/push-repo.sh
+++ build/push-repo.sh
@@ -6,7 +6,7 @@ git config --global user.name "Github Actions"
 git config --global user.email "development@kvalitetsit.dk"
 git clone https://github.com/KvalitetsIT/helm-repo
 cd helm-repo
-cp ../../krakend-*.tgz service
+cp ../../krakend-*.tgz krakend
 helm repo index . --url https://raw.githubusercontent.com/KvalitetsIT/helm-repo/master/
 git add -A
 git commit -m "Adding new krakend chart"
